6b45a48b01200d4d1f7cc0711816062e14b0260b
Nikolay S <sirotkin.nikola@mail.ru>
2025-11-18 16:03:04 -0500

    create get district

--- alembic/README.md
+++ alembic/README.md
@@ -4,6 +4,7 @@ Generic single-database configuration.
 2. Сделать миграции  `docker compose exec app alembic revision --autogenerate -m "add user table"`
 3. Скопировать миграции из контейнера в проект  `docker cp fastapi_app:/app/alembic/versions/ ./alembic/versions/`
 4. Остановить все контейнеры  `docker compose down`
-5. Сборка контейнера  `docker compose build --no-cache app`
-6. Просмотр логов приложения  `docker compose logs -f --timestamps app` или `db`
-7. Если возникли проблемы с остановкой и удалением контейнера: `sudo aa-remove-unknown` 
+5. Сборка контейнера  `docker compose build --no-cache`
+6. Запуск контейнера  `docker compose up -d`
+7. Просмотр логов приложения  `docker compose logs -f --timestamps app` или `db`
+8. Если возникли проблемы с остановкой и удалением контейнера: `sudo aa-remove-unknown` 
--- app/api/v1.py
+++ app/api/v1.py
@@ -2,6 +2,20 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app import models, schemas, crud
 from app.deps import get_db
+from app.driver import get_driver
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+"""
+----------- Производительность и ресурсы -----------
+Создание и закрытие Chrome для каждого запроса — дорого. Для реального использования
+можно использовать пул драйверов (внешняя очередь/брокер) — сложнее.
+Использовать Selenium Grid / Селениум-узлы, чтобы несколько клиентов могли переиспользовать сессии.
+Если вы делаете много автоматизации, подумайте об очереди задач (Celery/RQ) и воркерах, которые держат драйверы постоянно.
+"""
+
 
 router = APIRouter()
 
@@ -12,8 +26,18 @@ def create_user(user_in: schemas.UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db, user_in)
 
-@router.get("/users")
-def list_users(db: Session = Depends(get_db)):
-    if not crud.get_users(db):
-        return 'No users in DB'
-    return crud.get_users(db)
+
+@router.get("/district")
+def use_driver(driver: WebDriver = Depends(get_driver)):
+    try:
+        driver.get("https://gorzdrav.spb.ru/service-free-schedule")
+        wait = WebDriverWait(driver, 10)
+        district_buttons = wait.until(
+            EC.presence_of_all_elements_located((By.XPATH,
+                '/html/body/div/div[1]/div[12]/div[3]/div[1]/div[2]/div[1]/div/div[1]/ul/li'
+            ))
+        )
+        district_buttons_texts = [btn.text for btn in district_buttons]
+        return {"district_buttons": district_buttons_texts}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
